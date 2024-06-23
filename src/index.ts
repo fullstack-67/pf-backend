36e0a629a9e6393b8d52f92af32ae4219291b106
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-06-23 17:40:49 +0700

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import "dotenv/config";
-import express from "express";
+import express, { ErrorRequestHandler } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import { dbClient } from "@db/client";
+import { todoTable } from "@db/schema";
+import { eq } from "drizzle-orm";
 
 //Intializing the express app
 const app = express();
@@ -12,20 +14,75 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-//
+// Query
 app.get("/todo", async (req, res, next) => {
   try {
     const results = await dbClient.query.todoTable.findMany();
-    console.log(results);
     res.json(results);
   } catch (err) {
     next(err);
   }
 });
 
+// Insert
+app.put("/todo", async (req, res, next) => {
+  try {
+    const todoText = req.body.todoText ?? "";
+    if (!todoText) throw new Error("Empty todoText");
+    await dbClient.insert(todoTable).values({
+      todoText,
+    });
+    res.json({ msg: `Insert successfully`, data: { todoText } });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Update
+app.patch("/todo", async (req, res, next) => {
+  try {
+    const id = req.body.id ?? "";
+    const todoText = req.body.todoText ?? "";
+    if (!todoText || !id) throw new Error("Empty todoText or id");
+    const result = await dbClient
+      .update(todoTable)
+      .set({ todoText })
+      .where(eq(todoTable.id, id))
+      .returning({ id: todoTable.id, todoText: todoTable.todoText });
+    res.json({ msg: `Update successfully`, data: result });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Delete
+app.delete("/todo", async (req, res, next) => {
+  try {
+    const id = req.body.id ?? "";
+    if (!id) throw new Error("Empty id");
+
+    const results = await dbClient.query.todoTable.findMany({
+      where: eq(todoTable.id, id),
+    });
+    if (results.length === 0) throw new Error("Invalid id");
+
+    console.log({ results });
+    await dbClient.delete(todoTable).where(eq(todoTable.id, id));
+    res.json({
+      msg: `Delete successfully`,
+      data: { id },
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
 // JSON Error Middleware
-const jsonErrorHandler = (err, req, res, next) => {
-  res.status(500).send({ error: err });
+const jsonErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let serializedError = JSON.stringify(err, Object.getOwnPropertyNames(err));
+  serializedError = serializedError.replace(/\/+/g, "/");
+  serializedError = serializedError.replace(/\\+/g, "/");
+  res.status(500).send({ error: serializedError });
 };
 app.use(jsonErrorHandler);
 
